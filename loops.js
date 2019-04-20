f3d75a1e10b286476e146c32792f1bbc2c2c5149
Lawson Hung <lhung1@babson.edu>
2019-04-20 02:16:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -10,9 +10,9 @@ function forLoop(array){
 }
 
 function whileLoop(n) {
-  if (n === 0) {
-    return 'done';
-  } else {
+  if (n > 0) {
     console.log(--n);
+  } else {
+    return 'done';
   }
 }
