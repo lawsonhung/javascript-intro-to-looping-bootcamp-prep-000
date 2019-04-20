6ccea858e70ee5bb8a5981f71618043b40107f41
Lawson Hung <lhung1@babson.edu>
2019-04-20 01:45:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      array.push("I am ${i} strange loop.");
+      array.concat("I am ${i} strange loop.");
     } else {
-      array.push("I am ${i} strange loops.");
+      array.concat("I am ${i} strange loops.");
     }
   }
 }
